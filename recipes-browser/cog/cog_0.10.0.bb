Igalia/meta-webkit

commit f98f5c85e77d912c65501f3077ba17ce4e47d1f3

cog: Bump up version to 0.10.0
Release notes: https://wpewebkit.org/release/cog-0.10.0.html

Signed-off-by: Pablo Saavedra <psaavedra@igalia.com>

recipes-browser/cog/cog_0.10.0.bb
@@ -1,0 +1,9 @@
+require cog.inc
+require conf/include/devupstream.inc
+
+SRC_URI[sha256sum] = "2c72369c636ca4684370adad1344071b23c9ee2c851eb7d738fa2e1d7092031f"
+
+SRC_URI_class-devupstream = "git://github.com/Igalia/cog.git;protocol=https;branch=master"
+SRCREV_class-devupstream = "10f0360cd1ef9f8258d7d4a06d4ab113f5635995"
+
+RDEPENDS_${PN} += "wpewebkit (>= 2.28)"

recipes-browser/cog/cog.inc
@@ -9,6 +9,8 @@ BUGTRACKER = "https://github.com/Igalia/cog/issues"
 LICENSE = "MIT"
 LIC_FILES_CHKSUM = "file://COPYING;md5=bf1229cd7425b302d60cdb641b0ce5fb"
 
+SRC_URI = "https://wpewebkit.org/releases/${P}.tar.xz"
+
 # Depend on wpewebkit unless the webkitgtk packageconfig option is selected.
 DEPENDS = " \
             ${@bb.utils.contains('PACKAGECONFIG', 'webkitgtk', 'webkitgtk', 'wpewebkit', d)} \

recipes-browser/cog/cog_0.8.1.bb
@@ -1,11 +1,6 @@
 require cog.inc
 require conf/include/devupstream.inc
 
-SRC_URI = "https://github.com/Igalia/cog/releases/download/v${PV}/cog-${PV}.tar.xz"
-
 SRC_URI[sha256sum] = "b82e917eb764943b9859c631974f8f0e748b79ae87bb7a944f46c818740e0208"
-
-SRC_URI_class-devupstream = "git://github.com/Igalia/cog.git;protocol=https;branch=master"
-SRCREV_class-devupstream = "e21e32901c66a22d2fda7ded19b1a4937dc2f5ad"
 
 RDEPENDS_${PN} += "wpewebkit (>= 2.28)"

commit 9b8536f3d2ed6a6ef805f7b7411dfdd8a1a971c6

cog,wpewebkit: Fix build regression for devupstream build
Regression introduced in f98f5c85e77d912c65501f3077ba17ce4e47d1f3

Unreviewed change.

Signed-off-by: Pablo Saavedra <psaavedra@igalia.com>

recipes-browser/cog/cog_0.10.0.bb
@@ -9,4 +9,4 @@ PACKAGECONFIG[headless] = "-DCOG_PLATFORM_HEADLESS=ON,-DCOG_PLATFORM_HEADLESS=OF
 SRC_URI_class-devupstream = "git://github.com/Igalia/cog.git;protocol=https;branch=master"
 SRCREV_class-devupstream = "10f0360cd1ef9f8258d7d4a06d4ab113f5635995"
 
-RDEPENDS_${PN} += "wpewebkit (>= 2.28)"
+RDEPENDS_${PN} += "wpewebkit (>= 2.32)"

recipes-browser/wpewebkit/wpewebkit_2.30.6.bb
@@ -1,5 +1,4 @@
 require wpewebkit.inc
-require conf/include/devupstream.inc
 
 FILESEXTRAPATHS_prepend := "${THISDIR}/${PN}:"
 