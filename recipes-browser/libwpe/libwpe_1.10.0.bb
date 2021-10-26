Igalia/meta-webkit

commit aee05d09c87de625b7232b4b5eae0624e96e17ff

libwpe: Bump up version to 1.12.0
SRCREV_class-devupstream was set to the latest commitish on master.

recipes-browser/libwpe/libwpe_1.10.0.bb
@@ -1,11 +1,7 @@
 require libwpe.inc
-require conf/include/devupstream.inc
 
 LICENSE = "BSD"
 LIC_FILES_CHKSUM = "file://COPYING;md5=371a616eb4903c6cb79e9893a5f615cc"
 
-SRC_URI[sha256sum] = "2415e270d45e3595ed4052bc105f733744dc2d3677e12ff4a831e5029841084d"
-
-SRC_URI_class-devupstream = "git://github.com/WebPlatformForEmbedded/libwpe.git;protocol=https;branch=master"
-SRCREV_class-devupstream = "4bdc57a8de3675aa0fa4a033f1ac89f792d36b94"
+SRC_URI[sha256sum] = "e8eeca228a6b4c36294cfb63f7d3ba9ada47a430904a5a973b3c99c96a44c18c"
 

recipes-browser/libwpe/libwpe_1.12.0.bb
@@ -1,0 +1,11 @@
+require libwpe.inc
+require conf/include/devupstream.inc
+
+LICENSE = "BSD"
+LIC_FILES_CHKSUM = "file://COPYING;md5=371a616eb4903c6cb79e9893a5f615cc"
+
+SRC_URI[sha256sum] = "e8eeca228a6b4c36294cfb63f7d3ba9ada47a430904a5a973b3c99c96a44c18c"
+
+SRC_URI_class-devupstream = "git://github.com/WebPlatformForEmbedded/libwpe.git;protocol=https;branch=master"
+SRCREV_class-devupstream = "e2a90a50857f4095de7e763cc6adc8540275ff49"
+

commit 311441429750368a2f920d4862330f65d5cba309

libwpe: fix checksum for version 1.10.0 tarball
This fixes a regression introduced in
aee05d09c87de625b7232b4b5eae0624e96e17ff.

recipes-browser/libwpe/libwpe_1.10.0.bb
@@ -3,5 +3,5 @@ require libwpe.inc
 LICENSE = "BSD"
 LIC_FILES_CHKSUM = "file://COPYING;md5=371a616eb4903c6cb79e9893a5f615cc"
 
-SRC_URI[sha256sum] = "e8eeca228a6b4c36294cfb63f7d3ba9ada47a430904a5a973b3c99c96a44c18c"
+SRC_URI[sha256sum] = "2415e270d45e3595ed4052bc105f733744dc2d3677e12ff4a831e5029841084d"
 