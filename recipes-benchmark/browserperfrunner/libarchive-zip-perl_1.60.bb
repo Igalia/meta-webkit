Igalia/meta-webkit

commit 3b915e656bd271b3f144816f7243bcc78393f22b

recipes: Remove invalid source directory assignments
This makes the recipes compatible with the `Switch UNPACKDIR to
a subdir of WORKDIR` from Poky:

  commit d24a7d0fb16457e10e7a216d4c9ae3fb265113d1
  Author: Richard Purdie <richard.purdie@linuxfoundation.org>
  Date:   Thu May 9 17:01:27 2024 +0100

    base: Switch UNPACKDIR to a subdir of WORKDIR

The patch also adds anonymous functions to keep the S definitions
for backward compatibility with scarthgap.

Change-Type: major

recipes-benchmark/browserperfrunner/libarchive-zip-perl_1.60.bb
@@ -8,7 +8,13 @@ LIC_FILES_CHKSUM = "file://META.yml;;beginline=13;endline=13;md5=963ce2822834787
 
 SRC_URI = "https://cpan.metacpan.org/authors/id/P/PH/PHRED/Archive-Zip-${PV}.tar.gz"
 
-S = "${WORKDIR}/Archive-Zip-${PV}"
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('UNPACKDIR', d.getVar('WORKDIR'))
+}
+
+S = "${UNPACKDIR}/Archive-Zip-${PV}"
 
 # crc32 at least depends on perl-module-lib, perl-module-cwd, perl-module-zip* ...
 # so let's just depend on the perl-modules meta-package for simplicity.

recipes-graphics/openxr/openxr_1.0.26.bb
@@ -6,7 +6,14 @@ inherit cmake pkgconfig
 
 SRC_URI = "https://github.com/KhronosGroup/OpenXR-SDK/archive/refs/tags/release-${PV}.tar.gz"
 SRC_URI[sha256sum] = "07e3e819af786c2a2bea16e8ec5243ce4e9d2876923112898fa4ea6dbd9bdd73"
-S = "${WORKDIR}/OpenXR-SDK-release-${PV}"
+
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('UNPACKDIR', d.getVar('WORKDIR'))
+}
+
+S = "${UNPACKDIR}/OpenXR-SDK-release-${PV}"
 
 LICENSE = "Apache-2.0"
 LIC_FILES_CHKSUM = "file://LICENSE;md5=3b83ef96387f14655fc854ddc3c6bd57"

recipes-browser/webkitgtk/webkitgtk_2.50.0.bb
@@ -54,8 +54,6 @@ RRECOMMENDS:${PN}-bin = "adwaita-icon-theme librsvg-gtk"
 
 inherit cmake lib_package pkgconfig perlnative python3native
 
-S = "${WORKDIR}/webkitgtk-${PV}"
-
 # To build with embedded gl support -> Enable *both* "opengl" and "gles2" option
 # To build with desktop  gl support -> Enable "opengl", but disable "gles2" option
 PACKAGECONFIG ??= " ${@bb.utils.contains('DISTRO_FEATURES', 'x11', 'x11', '', d)} \

recipes-browser/wpebackend-rdk/wpebackend-rdk_1.20230523.bb
@@ -7,7 +7,11 @@ SRCREV = "108e4ab0da043421202e3ef64e0a38d1db8b82ee"
 
 SRC_URI = "git://github.com/WebPlatformForEmbedded/WPEBackend-rdk.git;protocol=https;branch=master"
 
-S = "${WORKDIR}/git"
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('S', d.getVar('WORKDIR') + '/git')
+}
 
 BBCLASSEXTEND += "devupstream:target"
 

recipes-cdm/sparkle-cdm/sparkle-cdm_git.bb
@@ -13,7 +13,11 @@ PV = "2021.0"
 SRC_URI = "git://github.com/Sparkle-CDM/sparkle-cdm.git;protocol=https;branch=main"
 SRCREV = "ce99d205e0423ab213869da5e934c0c1cd9983a8"
 
-S = "${WORKDIR}/git"
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('S', d.getVar('WORKDIR') + '/git')
+}
 
 PACKAGECONFIG ?= ""
 PACKAGECONFIG[sample-player] = "-Dsample-player=enabled,-Dsample-player=disabled,libsoup-2.4"

recipes-devtools/dicts/test-dicts_git.bb
@@ -7,7 +7,11 @@ BPV = "0.0.1"
 PV = "${BPV}"
 SRCREV = "a63aa52f09ddbf2532304a5751d22e5712299b0d"
 
-S = "${WORKDIR}/git"
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('S', d.getVar('WORKDIR') + '/git')
+}
 
 SRC_URI = " \
     git://github.com/mrobinson/webkitgtk-test-dicts.git;protocol=https;branch=master \

recipes-devtools/fonts/webkit-test-fonts_git.bb
@@ -7,7 +7,11 @@ BPV = "0.0.8"
 PV = "${BPV}"
 SRCREV = "f6382ed93932cca128c8d9edec3088f85a7592d9"
 
-S = "${WORKDIR}/git"
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('S', d.getVar('WORKDIR') + '/git')
+}
 
 SRC_URI = " \
     git://github.com/WebKitGTK/webkitgtk-test-fonts.git;protocol=https;branch=master \

recipes-devtools/ruby/ruby-highline_git.bb
@@ -19,7 +19,11 @@ BPV = "1.7.3"
 PV = "${BPV}"
 SRCREV = "327051c1c217df2880c3a53f31484f7e815e847f"
 
-S = "${WORKDIR}/git"
+# Backward compatibility with scarthgap
+python __anonymous() {
+    if not d.getVar('UNPACKDIR'):
+        d.setVar('S', d.getVar('WORKDIR') + '/git')
+}
 
 RUBY_GEM_NAME="highline"
 RUBY_BUILD_GEMS="${RUBY_GEM_NAME}.gemspec"

commit f6db7417c468cc4007ff99fce2d62d75b85820c5

recipes: fix sstate re-usage by dropping anonymous functions
For backward compatibility with scarthgap an anonymous function to
re-set 'S' was introduced, which broke sstate-cache re-usage for these
packages.
On scarthgap builds, 'S' would now be directly set during recipe
parsing to an expanded and absolute path - instead of keeping the
usually late/lazy expanded variables.

Remove the anonymous python function, adapt the SRC_URI instead to
unpack the fetched tarball to the correct location, and otherwise
leave 'S' at the bitbake.conf defaults.
On master this would end up being:
UNPACKDIR ??= "${WORKDIR}/sources" with S = "${UNPACKDIR}/${BP}"
on scarthgap it would instead be:
S = "${WORKDIR}/${BP}"

Fixes: 3b915e6 recipes: Remove invalid source directory assignments
Signed-off-by: Johannes Schneider <johannes.schneider@leica-geosystems.com>

recipes-benchmark/browserperfrunner/libarchive-zip-perl_1.60.bb
@@ -6,15 +6,7 @@ SECTION = "libs"
 LICENSE = "Artistic-1.0 | GPL-1.0-or-later"
 LIC_FILES_CHKSUM = "file://META.yml;;beginline=13;endline=13;md5=963ce28228347875ace682de56eef8e8"
 
-SRC_URI = "https://cpan.metacpan.org/authors/id/P/PH/PHRED/Archive-Zip-${PV}.tar.gz"
-
-# nooelint: oelint.task.noanonpython - required for backward compatibility with scarthgap
-python __anonymous() {
-    if not d.getVar('UNPACKDIR'):
-        d.setVar('UNPACKDIR', d.getVar('WORKDIR'))
-}
-
-S = "${UNPACKDIR}/Archive-Zip-${PV}"
+SRC_URI = "https://cpan.metacpan.org/authors/id/P/PH/PHRED/Archive-Zip-${PV}.tar.gz;subdir=${BP};striplevel=1"
 
 # crc32 at least depends on perl-module-lib, perl-module-cwd, perl-module-zip* ...
 # so let's just depend on the perl-modules meta-package for simplicity.

recipes-browser/webkitgtk/webkitgtk_2.50.4.bb
@@ -42,7 +42,7 @@ DEPENDS = "curl \
            zlib \
 "
 
-SRC_URI = "https://www.webkitgtk.org/releases/webkitgtk-${PV}.tar.xz;name=tarball"
+SRC_URI = "https://www.webkitgtk.org/releases/webkitgtk-${PV}.tar.xz;name=tarball;subdir=${BP};striplevel=1"
 SRC_URI[tarball.sha256sum] = "d3bfa473845acfab72635bada5e0d134fda6792c5b95c5c5cd141b46125bd8e4"
 
 inherit cmake lib_package pkgconfig perlnative python3native

recipes-graphics/openxr/openxr_1.0.26.bb
@@ -13,16 +13,8 @@ LIC_FILES_CHKSUM = "file://LICENSE;md5=3b83ef96387f14655fc854ddc3c6bd57"
 
 DEPENDS:append = " jsoncpp"
 
-SRC_URI = "https://github.com/KhronosGroup/OpenXR-SDK/archive/refs/tags/release-${PV}.tar.gz"
+SRC_URI = "https://github.com/KhronosGroup/OpenXR-SDK/archive/refs/tags/release-${PV}.tar.gz;subdir=${BP};striplevel=1"
 SRC_URI[sha256sum] = "07e3e819af786c2a2bea16e8ec5243ce4e9d2876923112898fa4ea6dbd9bdd73"
-
-# nooelint: oelint.task.noanonpython - required for backward compatibility with scarthgap
-python __anonymous() {
-    if not d.getVar('UNPACKDIR'):
-        d.setVar('UNPACKDIR', d.getVar('WORKDIR'))
-}
-
-S = "${UNPACKDIR}/OpenXR-SDK-release-${PV}"
 
 # The CMake system of this package is a bit convoluted. It tries to enable
 # features automatically based on the dependencies it detects...